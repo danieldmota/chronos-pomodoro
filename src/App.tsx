f150c0f5a614b945fc4243b8e8926685207e64ea
danieldmota <danieldourado2102@gmail.com>
2025-09-29 17:13:31 -0400

    components button e footer

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 
 export function App() {
   return (
@@ -34,12 +37,14 @@ export function App() {
           </div>
           <div className='formRow'>
             <Cycles/>
-            <p>0 0 0 0 0 0</p>
           </div>
           <div className='formRow'>
-            <button>Enviar</button>
+            <DefaultButton icon={<PlayCircleIcon/>} color='green'/>
           </div>
         </form>
+      </Container>
+      <Container>
+        <Footer />
       </Container>
     </>
   );
