b226a07b3416dda8665e26c785b9f02928ae189e
danieldmota <danieldourado2102@gmail.com>
2025-09-29 16:41:03 -0400

    component de ciclos

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
 
 export function App() {
   return (
@@ -32,7 +33,7 @@ export function App() {
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
           <div className='formRow'>
-            <p>Ciclos</p>
+            <Cycles/>
             <p>0 0 0 0 0 0</p>
           </div>
           <div className='formRow'>
