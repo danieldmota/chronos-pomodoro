f265dd1e0e89994300dad0051d7308e6ab4add6d
danieldmota <danieldourado2102@gmail.com>
2025-10-18 18:21:06 -0400

    fazendo algumas pages e generic html

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { Home } from './pages/Home';
+
 import './styles/theme.css';
 import './styles/global.css';
+import { NotFound } from './pages/NotFound';
+import { AboutPomodoro } from './pages/AboutPomodoro';
 
 export function App() {
-  return <Home />;
+  return <AboutPomodoro />;
 }
